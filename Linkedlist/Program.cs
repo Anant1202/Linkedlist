5f4b35655aeee892ffaf5d0e81bf8718739f52d2
Anant1202 <anantsingh1202@gmail.com>
2022-05-23 16:27:44 +0530

    Welcome to Linked List Program

---
+++ Linkedlist/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Linkedlist
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked list Program");
+        }
+    }
+}
+
