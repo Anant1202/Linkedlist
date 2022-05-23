bdffb6d8f8faf2bf29bb6da8e4063a719683429d
Anant1202 <anantsingh1202@gmail.com>
2022-05-23 18:19:08 +0530

    Added:Adding List from the Last

---
+++ Linkedlist/LinkedlistOperations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linkedlist
+{
+    public class LinkedlistOperations
+    {
+        Node head;
+        public void AddLast(int data)
+        {
+            Node new_node = new Node(data);
+            if (head == null)
+            {
+                head = new_node;
+                return;
+            }
+            Node lastNode = head;
+            while (lastNode.next != null)
+            {
+                lastNode = lastNode.next;
+            }
+            lastNode.next = new_node;
+        }
+        public void Print()
+        {
+            Node lastNode=head;
+            Console.Write("Simple Linked list :");
+            while (lastNode!= null)
+            {
+                Console.Write(lastNode.data + "->"); 
+                lastNode = lastNode.next;
+            }
+            Console.Write("null\n");
+        }
+    }
+}
--- Linkedlist/Program.cs
+++ Linkedlist/Program.cs
@@ -7,6 +7,11 @@ namespace Linkedlist
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Linked list Program");
+            LinkedlistOperations obj = new LinkedlistOperations();
+            obj.AddLast(56);
+            obj.AddLast(30);
+            obj.AddLast(70);
+            obj.Print();
         }
     }
 }
